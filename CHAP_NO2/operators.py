9f3fb9d9f4410436fa744a55051a41a16cb19343
Abdul Muneeb Swati <muneebswati00@gmail.com>
2024-09-24 02:00:07 +0500

    first commit

---
+++ CHAP_NO2/operators.py
@@ -0,0 +1,14 @@
+meaning = 9
+
+meaning - 10
+print(meaning)
+if meaning > 10:
+    print('Right on!')
+else:
+    print('Not today')
+
+# Ternary Operator
+print('Right on!') if meaning > 10  else print('Not today')
+
+
+
