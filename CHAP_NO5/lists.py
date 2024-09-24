d778b9ae05413ba19a44b726edff8366f00e8c68
Abdulmuneebswati <muneebswati00@gmail.com>
2024-09-24 19:14:46 +0500

    lists added

---
+++ CHAP_NO5/lists.py
@@ -0,0 +1,102 @@
+# # users = ["Ali","Hassan","Hasseb"]
+# # data =["Muneeb","32","adult"]
+
+# # emptyList =[]
+
+# # print("Muneeb" in data)
+# # print(users[-1])
+
+# # print(users.index('Ali'))
+# # print(users[0:2]) #['Ali', 'Hassan']
+# # print(users[0:3]) #['Ali', 'Hassan', 'Hasseb']
+# # print(users[1:2]) #['Hassan']
+
+
+# # print(len(data))
+
+
+# # users.append('jawad')
+# # print(users)
+
+# # users += ['kafi']
+# # print(users)
+
+# # users.extend(['Robert', 'Jimmy'])
+# # print(users)
+
+# # users.insert(0, 'Bob')
+# # print(users)
+
+# # users[2:2] = ['Eddie', 'Alex']
+# # print(users)
+
+# # users[1:3] = ['Robert', 'JPJ']
+# # print(users)
+
+# # users.remove('Bob')
+# # print(users)
+# # print(users.pop())
+# # print(users)
+
+# # del users[0]
+# # print(users)
+
+
+# # data.clear()
+# # print(data)
+
+
+# # users[1:2] = ['dave']
+# # print(users)
+# # users.sort()
+# # print(users)
+
+
+# # nums = [4, 42, 78, 1, 5]
+# # nums.reverse()
+# # print(nums)
+
+# # nums.sort(reverse=True)
+# # print(nums)
+
+# # print(sorted(nums, reverse=True))
+# # print(nums)
+
+# # numscopy = nums.copy()
+# # mynums = list(nums)
+# # mycopy = nums[:]
+
+# # print(numscopy)
+# # print(mynums)
+# # print(mycopy)
+
+
+# # mycopy.sort()
+# # print(mycopy)
+# # print(nums)
+
+# # print(type(nums))
+
+# mylist = list([1, "Neil", True])
+
+# mytuple = tuple(('Dave', 42, True))
+
+# anothertuple = (1, 4, 2, 8, 2, 2)
+
+
+# print(mytuple)
+# print(type(mytuple))
+# print(type(anothertuple))
+
+# newlist = list(mytuple)
+# newlist.append('Neil')
+# print(newlist,"newlist")
+# newtuple = tuple(newlist)
+# print(newtuple,"newtuple")
+
+# (one, *two, hey) = anothertuple
+# print(one)
+# print(two)
+# print(hey)
+
+# print(anothertuple.count(2))
