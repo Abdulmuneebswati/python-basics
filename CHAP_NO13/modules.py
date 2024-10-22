04a002151d1855601333b7925819328cdbff2887
Abdulmuneebswati <muneebswati00@gmail.com>
2024-10-22 22:54:15 +0500

    modules in python

---
+++ CHAP_NO13/modules.py
@@ -0,0 +1,17 @@
+import math
+from enum import Enum
+from math import pi
+import random as rdm
+import kansas
+# print(pi)
+# print(rdm.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+# print(dir(rdm))
+# for item in dir(rdm):
+#     print(item)
+
+# kansas.randomfunfact()
+# print(dir(kansas))
+
+# for item in dir(kansas):
+#     print(item)
+# print(__name__)
